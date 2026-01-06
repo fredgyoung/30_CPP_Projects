6b486eafe65dbbf30eec1c2807dd0b50888482e4
Fred Young <fredgyoung@gmail.com>
2026-01-05 23:40:52 -0500

    Added average

--- CGPA_Calculator/CGPA_Calculator.cpp
+++ CGPA_Calculator/CGPA_Calculator.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 int main() {
 
     cout << "Calculator!" << endl;
-
     vector<double> grades = {2, 4, 8, 5};
 
     for (const double grade : grades) {
-        cout << grade << ", ";
+        cout << grade << " ";
     }
+    cout << endl;
+
+    // Use 0.0 as the initial value to ensure the sum is accumulated as a double
+    const double sum = accumulate(grades.begin(), grades.end(), 0.0);
+    // Divide the sum by the number of elements
+    double avg = sum / grades.size();
+    cout << "Average: " << avg << endl;
 
     return 0;
 }
