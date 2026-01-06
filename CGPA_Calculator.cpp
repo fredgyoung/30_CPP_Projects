bf3bdc805484c7e64f5a8cbf9ee97086ae84188a
Fred Young <fredgyoung@gmail.com>
2026-01-05 23:30:01 -0500

    Initial commit

---
+++ CGPA_Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    cout << "Calculator!" << endl;
+    
+    vector<double> grades = {2, 4, 8, 5};
+
+    for (const double grade : grades) {
+        cout << grade << ", ";
+    }
+
+    return 0;
+}
